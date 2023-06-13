49ec084b42be942347dff1b4dedb12a55e7417d1
Parker <pp101socials@gmail.com>
2023-06-13 15:45:34 -0400

    Added widgets

--- frontend/windows/StartWindow.py
+++ frontend/windows/StartWindow.py
@@ -12,8 +12,9 @@ from PyQt5.QtCore import *
 from backend.database.classes.Connection import db_connection
 
 from frontend.assets.variables.arrays import active_connections
-from frontend.assets.qrcs import light_bg, dark_bg, settings_icon, light_mode_icon, dark_mode_icon
+from frontend.assets.qrcs import light_bg, dark_bg, settings_icon, light_mode_icon, dark_mode_icon, start_window_bg
 from frontend.assets.functions.ShadowText import *
+from frontend.assets.functions.DisplayRequirementsText import display_success_error_label
 
 import sys, pyodbc, atexit
 
@@ -25,7 +26,7 @@ class StartWindow(QMainWindow):
         super(StartWindow, self).__init__()
 
         uic.loadUi("frontend/ui/StartWindow.ui", self)
-
+        self.setWindowFlag(Qt.FramelessWindowHint)
 
         # Define widgets
         # EX: self.testWidget = self.findChild(QLineEdit, "startWindow_TestLE")
@@ -34,6 +35,12 @@ class StartWindow(QMainWindow):
         self.textLayout = self.findChild(QVBoxLayout, "StartWindow_TextLayout")
         self.mainHeadingLabel = self.findChild(QLabel, "StartWindow_MainHeadingLabel")
         self.mainTextLabel = self.findChild(QLabel, "StartWindow_MainTextLabel")
+        self.requirementsLabel = self.findChild(QLabel, "StartWindow_RequirementsLabel")
+        self.registerBtn = self.findChild(QPushButton, "StartWindow_RegisterBtn")
+        self.loginBtn = self.findChild(QPushButton, "StartWindow_LoginBtn")
+        self.exitBtn = self.findChild(QPushButton, "StartWindow_ExitBtn")
+        self.topFrame = self.findChild(QFrame, "StartWindow_TopFrame")
+        self.bottomFrame = self.findChild(QFrame, "StartWindow_BottomFrame")
 
         headingShadow = QGraphicsDropShadowEffect()
         applyBlur(headingShadow, 15)
@@ -45,8 +52,32 @@ class StartWindow(QMainWindow):
         changeShadowColor(textShadow, QColor(38, 38, 38))
         positionShadow(textShadow, 0, 2)
 
+        registerShadow = QGraphicsDropShadowEffect()
+        applyBlur(registerShadow, 15)
+        changeShadowColor(registerShadow, QColor(97, 69, 50))
+        positionShadow(registerShadow, 0, 2)
+
+        loginShadow = QGraphicsDropShadowEffect()
+        applyBlur(loginShadow, 15)
+        changeShadowColor(loginShadow, QColor(97, 69, 50))
+        positionShadow(loginShadow, 0, 2)
+
+        exitShadow = QGraphicsDropShadowEffect()
+        applyBlur(exitShadow, 15)
+        changeShadowColor(exitShadow, QColor(97, 69, 50))
+        positionShadow(exitShadow, 0, 2)
+
+        requirementsShadow = QGraphicsDropShadowEffect()
+        applyBlur(requirementsShadow, 15)
+        changeShadowColor(requirementsShadow, QColor(38, 38, 38))
+        positionShadow(requirementsShadow, 0, 2)
+
         self.mainHeadingLabel.setGraphicsEffect(headingShadow)
         self.mainTextLabel.setGraphicsEffect(textShadow)
+        self.requirementsLabel.setGraphicsEffect(requirementsShadow)
+        self.registerBtn.setGraphicsEffect(registerShadow)
+        self.loginBtn.setGraphicsEffect(loginShadow)
+        self.exitBtn.setGraphicsEffect(exitShadow)
 
         # Define functions
         # EX: def doSomething():
@@ -59,7 +90,8 @@ class StartWindow(QMainWindow):
             :return:
             '''
 
-            self.setStyleSheet("QMainWindow {border-image: url('frontend/assets/imgs/light-mode-bg.png')}")
+            self.topFrame.setStyleSheet("QFrame {border-image: url('frontend/assets/imgs/light-mode-bg.png')}")
+            self.bottomFrame.setStyleSheet("QFrame {border-image: url('frontend/assets/imgs/light-mode-bg.png')}")
 
             self.lightModeToggler.setFixedWidth(0)
             self.darkModeToggler.setFixedWidth(45)
@@ -72,7 +104,8 @@ class StartWindow(QMainWindow):
             :return:
             '''
 
-            self.setStyleSheet("QMainWindow {border-image: url('frontend/assets/imgs/dark-mode-bg.png')}")
+            self.topFrame.setStyleSheet("QFrame {border-image: url('frontend/assets/imgs/dark-mode-bg.png')}")
+            self.bottomFrame.setStyleSheet("QFrame {border-image: url('frontend/assets/imgs/dark-mode-bg.png')}")
 
             self.lightModeToggler.setFixedWidth(45)
             self.darkModeToggler.setFixedWidth(0)
@@ -93,8 +126,10 @@ class StartWindow(QMainWindow):
         # Apply functions to/style widgets
         self.lightModeToggler.mousePressEvent = lambda event: toggleLightMode()
         self.darkModeToggler.mousePressEvent = lambda event: toggleDarkMode()
+        self.exitBtn.clicked.connect(exitApp)
 
         # Displaying result of program detection
+        display_success_error_label(self)
 
         # Show the app
         self.show()
